8e891af582dcb5f5e89fd20401aa917301fddfce
kotoyama <kotoyama@list.ru>
2021-11-20 03:11:30 +0500

    refactor(entities): add comments for types fields

--- src/entities/api/tickets.ts
+++ src/entities/api/tickets.ts
@@ -7,17 +7,26 @@ export interface SearchIdEntity {
 }
 
 export interface SegmentEntity {
+  // Код города (iata)
   origin: string
+  // Код города (iata)
   destination: string
+  // Дата и время вылета туда
   date: string
+  // Массив кодов (iata) городов с пересадками
   stops: string[]
+  // Общее время перелёта в минутах
   duration: number
 }
 
 export interface TicketEntity {
+  // Цена в рублях
   price: number
+  // Код авиакомпании (iata)
   carrier: string
-  segments: SegmentEntity[]
+  // Массив перелётов
+  // В тестовом задании это всегда поиск "туда-обратно", значит, состоит из двух элементов
+  segments: [SegmentEntity, SegmentEntity]
 }
 
 export interface TicketsEntity {
