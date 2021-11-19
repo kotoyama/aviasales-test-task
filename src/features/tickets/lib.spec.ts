f7f07d1eca39837db3abda724b25ccd26f9e1b88
kotoyama <kotoyama@list.ru>
2021-11-20 02:28:04 +0500

    refactor(tickets): add datetime options for `getTimeInterval` method

--- src/features/tickets/lib.spec.ts
+++ src/features/tickets/lib.spec.ts
@@ -13,23 +13,23 @@ const mockTimezone = {
 }
 
 describe('tickets lib', () => {
-  describe('getTimeFromDate', () => {
-    test('should extract time from date properly', () => {
-      expect(getTimeFromDate(mockSegment.date, mockTimezone)).toBe('05:18')
-    })
-  })
-
   describe('formatDuration', () => {
     test('should format flight duration properly', () => {
       expect(formatDuration(mockSegment.duration)).toBe('29ч 45м')
     })
   })
 
+  describe('getTimeFromDate', () => {
+    test('should extract time from date properly', () => {
+      expect(getTimeFromDate(mockSegment.date, mockTimezone)).toBe('05:18')
+    })
+  })
+
   describe('getTimeInterval', () => {
     test('should calculate time interval properly', () => {
-      expect(getTimeInterval(mockSegment.date, mockSegment.duration)).toBe(
-        '05:18 – 11:03',
-      )
+      expect(
+        getTimeInterval(mockSegment.date, mockSegment.duration, mockTimezone),
+      ).toBe('05:18 – 11:03')
     })
   })
 })
--- src/features/tickets/lib.ts
+++ src/features/tickets/lib.ts
@@ -1,3 +1,9 @@
+export const formatDuration = (minutes: number): string => {
+  const hh = Math.floor(minutes / 60)
+  const mm = Math.round(minutes % 60)
+  return `${hh}ч ${mm}м`
+}
+
 export const getTimeFromDate = (
   date: string | number,
   options?: Intl.DateTimeFormatOptions,
@@ -8,14 +14,12 @@ export const getTimeFromDate = (
     ...options,
   })
 
-export const formatDuration = (minutes: number): string => {
-  const hh = Math.floor(minutes / 60)
-  const mm = Math.round(minutes % 60)
-  return `${hh}ч ${mm}м`
-}
-
-export const getTimeInterval = (date: string, duration: number): string => {
+export const getTimeInterval = (
+  date: string,
+  duration: number,
+  options?: Intl.DateTimeFormatOptions,
+): string => {
   const start = new Date(date).getTime()
   const end = start + 60 * 1000 * duration
-  return `${getTimeFromDate(start)} – ${getTimeFromDate(end)}`
+  return `${getTimeFromDate(start, options)} – ${getTimeFromDate(end, options)}`
 }
